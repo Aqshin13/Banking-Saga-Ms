592b610e62078c45b0c612f562df1520b75f3331
Aqshin13 <aqsin.quliyev.17@inbox.ru>
2026-02-18 21:48:26 +0400

    MS starting

---
+++ top-up-service/src/test/java/com/banking/TopUpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopUpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
